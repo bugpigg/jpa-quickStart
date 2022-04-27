986195041bcb7673d64465a8bdac7a2df5a44f83
bugpigg <bugpigg@gmail.com>
2022-04-27 16:50:38 +0900

    refact: 롬복 라이브러리 의존성 추가 및 적용

--- Chapter01/src/main/java/com.rubypaper.persistence.jdbc/EmployeeVO.java
+++ Chapter01/src/main/java/com.rubypaper.persistence.jdbc/EmployeeVO.java
@@ -1,7 +1,9 @@
 package com.rubypaper.persistence.jdbc;
 
 import java.sql.Timestamp;
+import lombok.Data;
 
+@Data
 public class EmployeeVO {
 
     private Long id;
@@ -11,63 +13,4 @@ public class EmployeeVO {
     private String deptName;
     private Double salary;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Timestamp getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Timestamp startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDeptName() {
-        return deptName;
-    }
-
-    public void setDeptName(String deptName) {
-        this.deptName = deptName;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
-
-    @Override
-    public String toString() {
-        return "EmployeeVO{" +
-            "id=" + id +
-            ", name='" + name + '\'' +
-            ", startDate=" + startDate +
-            ", title='" + title + '\'' +
-            ", deptName='" + deptName + '\'' +
-            ", salary=" + salary +
-            '}';
-    }
 }
